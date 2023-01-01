15e4d66dcb439747e2f822febbba1a72e3e5ae73
Taib <taibkurbanaliev@gmail.com>
2023-01-01 19:25:48 +0400

    Init

---
+++ Assets/Scripts/SpawnPoint.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+    [SerializeField] private Transform _generalPoint;
+    [SerializeField] private GameObject _prefab;
+
+    private Transform[] _points;
+    private int _delay = 2;
+
+    private void Start()
+    {
+        _points = new Transform[_generalPoint.childCount];
+
+        for(int i = 0; i < _points.Length; i++)
+        {
+            _points[i] = _generalPoint.GetChild(i).transform;
+        }
+
+        
+        StartCoroutine(SpawnPrefabs());
+    }
+
+    private IEnumerator SpawnPrefabs()
+    {
+        for(int i = 0; i < _points.Length; i++)
+        {
+            Instantiate(_prefab, _points[i].position, _points[i].rotation);
+            yield return new WaitForSeconds(_delay);
+        }
+    }
+}
